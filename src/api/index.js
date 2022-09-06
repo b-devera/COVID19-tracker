631b6427aefecd4754ab81be7a96a1c0941126bb
Berlun Devera <berlun_devera@icloud.com>
2022-09-05 20:02:04 -0700

    added functionality for country dropdown

--- src/api/index.js
+++ src/api/index.js
@@ -2,33 +2,38 @@ import axios from "axios";
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
-    try {
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+export const fetchData = async (country) => {
+  let urlChanger = url;
 
-        return {
-            confirmed,
-            recovered,
-            deaths,
-            lastUpdate,
-        };
-    } catch (error) {
-        
-    }
+  if (country) {
+    urlChanger = `${url}/countries/${country}`;
+  }
+  
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(urlChanger);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
 }
 
 export const fetchDailyData = async () => {
-    try {
-        const { data } = await axios.get(`${url}/daily`);
+  try {
+    const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }));
+    return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+  } catch (error) {
+    return error;
+  }
+}
 
-        return modifiedData;
-    } catch (error) {
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
 
-    }
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
 }
